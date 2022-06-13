4b5c7ab2f141de1b281ad5b6e0125482d153311c
Styp <stypjairo@gmail.com>
2022-06-13 12:23:45 -0500

    Add documentation

---
+++ 2021/css intermedio/12_flexbox/Practica/javascript/flexbox.js
@@ -0,0 +1,6 @@
+const menu = document.querySelector(".menu")
+const nav = document.querySelector(".nav")
+
+menu.addEventListener("click", function (){
+    document.getElementById(".menu").classList.toggle(".mostrar__menu")
+});
